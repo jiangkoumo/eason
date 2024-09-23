8919c3ecd1623d281c13d6be41e70c642d140827
jiangkoumo <2019932027@qq.com>
2024-09-23 23:23:03 +0800

    数组练习

---
+++ VS Code/9_23_array_.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// strlen函数所需的头文件
+#include <string.h>
+// Sleep所需的头文件
+#include <windows.h>
+
+int main()
+{
+    char arr1[] = "hello word";
+    char arr2[] = "**********";
+
+    int left = 0; 
+    //strlen函数计算数组长度。数组长度减一就是最后一个元素的坐标
+    int right = strlen(arr1)-1;
+    while (left <= right)
+    {
+        arr2[left] = arr1[left];
+        arr2[right] = arr1[right];
+
+        printf("%s\n", arr2);
+
+        // 停顿1000毫秒
+        Sleep(1000);
+
+        // 清理屏幕
+        // system("cls"); 
+
+        left++;
+        right--;
+    }
+
+    return 0;
+}
